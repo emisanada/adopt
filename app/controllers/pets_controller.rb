937f68e0b693f2db945a5785727aab55e1bed5db
Gabriela Emi Sanada <emisanada@hotmail.com>
2018-04-28 19:29:19 -0300

    Add pet for adoption

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,14 @@
 class PetsController < ApplicationController
 
+  before_action :check_user, only: [:new, :create, :create, :update, :delete]
+
   def index
+    @pets = Pet.all
   end
 
   def find
   end
-  
+
   def show
   end
 
@@ -13,6 +16,25 @@ class PetsController < ApplicationController
   end
 
   def create
+    params[:pet][:user_id] = @current_user.id
+    params[:pet][:status] = false
+    pet = Pet.create(pet_params)
+    if pet.errors.any?
+      pets_errors = []
+      pet.errors.full_messages.each do |message|
+        pets_errors << message
+      end
+    end
+    pet.save!
+    flash[:notice] = 'Pet listed for adoption successfully!'
+    redirect_to action: :index
+  rescue => e
+    flash[:error] = 'Ops! There was a problem on your pet form!'
+    if pets_errors.present?
+      flash[:error] = [flash[:error]] << pets_errors
+      flash[:error].flatten!
+    end
+    redirect_to action: :new
   end
 
   def edit
@@ -24,4 +46,8 @@ class PetsController < ApplicationController
   def delete
   end
 
+  private
+    def pet_params
+      params.require(:pet).permit(:name, :breed, :age, :location, :about, :status, :user_id)
+    end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,3 @@
 class Pet < ActiveRecord::Base
-
+  belongs_to :user
 end
