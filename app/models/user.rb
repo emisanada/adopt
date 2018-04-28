f7041b53b33a9d1c960d9e1c9fbbc740317a1114
Gabriela Emi Sanada <emisanada@hotmail.com>
2018-04-28 01:35:46 -0300

    Add password validations and encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
-class User < MoipActiveModel
+class User < ActiveRecord::Base
+  attr_accessor :password
+
+  before_validation :encrypt_password
+  after_save :clear_password
+
+  EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
+  validates :username, presence: true, uniqueness: true, length: { in: 3..25 }
+  validates :password, confirmation: true
+  validates_length_of :password, in: 6..20, on: :create
+
+  has_many :pets
+
+  private
+    def encrypt_password
+      if password.present?
+        self.salt = BCrypt::Engine.generate_salt
+        self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+      end
+    end
+
+    def clear_password
+      self.password = nil
+    end
 
 end
