e9639c8cf069907b62688755d955d2130cbb05c5
Gabriela Emi Sanada <emisanada@hotmail.com>
2018-04-28 01:34:56 -0300

    Create action create for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def find
@@ -13,6 +14,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    user.save
+    if user.save!
+      flash[:notice] = 'You signed up successfully'
+    else
+      flash[:error]= 'Ops! There was a problem on your signup!'
+    end
+    redirect_to root_path
+  rescue => e
+    Rails.logger.error "-------------> #{e.message}"
+    Rails.logger.error "-------------> #{e.backtrace.join("\n\t")}"
+    flash[:error]= 'invalid'
+    redirect_to root_path
   end
 
   def edit
@@ -23,5 +37,10 @@ class UsersController < ApplicationController
 
   def delete
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :location, :username, :password, :password_confirmation)
+    end
 
 end
